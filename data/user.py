fecc1888836731abead04cb2d27ae5d1b966f767
Nguyen Minh Tuan <ngminhtuan2303@gmail.com>
2023-05-31 21:02:32 +0700

    update

--- data/user.py
+++ data/user.py
@@ -1,23 +1,23 @@
+from schemas.user import User
 from datetime import datetime
-from schemas.user import User,UserCreate
 
-users_data = [
+# Global variable
+users = [
     User(
-        ho_ten="Nguyen Van A",
-        ngay_sinh=datetime.strptime("1990-01-01", "%Y-%m-%d"),
-        gioi_tinh="Nam",
-        so_dt="0123456789",
-        dia_chi="Ha Noi",
-        email="nguyenvana@example.com",
-        gioi_thieu="",
+        full_name="Nguyễn Văn A",
+        birthday=datetime.strptime("1990-01-01", "%Y-%m-%d"),
+        gender="Male",
+        phone_number="0123456789",
+        address="Hà Nội",
+        email="nguyenvana@gmail.com",
     ),
     User(
-        ho_ten="Le Thi B",
-        ngay_sinh=datetime.strptime("1995-02-01", "%Y-%m-%d"),
-        gioi_tinh="Nu",
-        so_dt="0987654321",
-        dia_chi="Ho Chi Minh",
-        email="lethib@example.com",
-        gioi_thieu="",
+        full_name="Lê Thị B",
+        birthday=datetime.strptime("1995-02-01", "%Y-%m-%d"),
+        gender="Female",
+        phone_number="0987654321",
+        address="Sài Gòn",
+        email="lethib@gmail.com",
+        introduction="I'm a software engineer",
     ),
 ]
--- services/user.py
+++ services/user.py
@@ -2,27 +2,7 @@ from typing import List
 from schemas.user import User, UserCreate, UserUpdate
 from datetime import datetime
 from fastapi import HTTPException
-
-# Global variable
-users = [
-    User(
-        full_name="Nguyễn Văn A",
-        birthday=datetime.strptime("1990-01-01", "%Y-%m-%d"),
-        gender="Male",
-        phone_number="0123456789",
-        address="Hà Nội",
-        email="nguyenvana@gmail.com",
-    ),
-    User(
-        full_name="Lê Thị B",
-        birthday=datetime.strptime("1995-02-01", "%Y-%m-%d"),
-        gender="Female",
-        phone_number="0987654321",
-        address="Sài Gòn",
-        email="lethib@gmail.com",
-        introduction="I'm a software engineer",
-    ),
-]
+from data.user import users
 
 
 class UserService:
